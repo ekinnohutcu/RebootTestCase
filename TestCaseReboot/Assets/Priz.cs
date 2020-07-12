5ec3758330a408a514f62bf223b52a9ae3fa357d
Ekin Nohutçu <60942915+ekinnohutcu@users.noreply.github.com>
2020-07-12 19:55:02 +0300

    case 2 added

---
+++ TestCaseReboot/Assets/Priz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Lean.Touch;
+using UnityEngine;
+
+public class Priz: MonoBehaviour
+{
+   
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Plug")
+        {
+            other.gameObject.GetComponent<PuzzlePiece>().isDone = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Plug")
+        {
+            other.gameObject.GetComponent<PuzzlePiece>().isDone = false;
+        }
+    }
+}
--- TestCaseReboot/Assets/Scripts/PlugManager.cs
+++ TestCaseReboot/Assets/Scripts/PlugManager.cs
@@ -1,22 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Lean.Touch;
 using UnityEngine;
 
 public class PlugManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private int _plugCount = 1;
+    
+    private int _plugCount = 0;
+
+
+    public GameObject winText;
+    public GameObject levelObjects;
+    public List<PuzzlePiece> piece;
+
+    private bool isFingerUp;
     
     
     
     void Start()
     {
-        
+        LeanTouch.OnFingerSet += OnFingerSet;
+        LeanTouch.OnFingerUp += OnFingerUp;
+        winText.SetActive(false);
+    }
+
+    private void OnFingerUp(LeanFinger finger)
+    {
+        isFingerUp = true;
+    }
+    
+    private void OnFingerSet(LeanFinger finger)
+    {
+        isFingerUp = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (var puzzlePiece in piece)
+        {
+            if (puzzlePiece.isDone && isFingerUp)
+            {
+                Win();
+            }
+        }
     }
+
+    public void Win()
+    {
+        levelObjects.SetActive(false);
+        winText.SetActive(true);
+    }
+    
+    
 }
